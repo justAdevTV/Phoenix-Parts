cf9ab583a06e1260cb9237763285d171d732cf25
Adam Garcia <adameitangarcia@gmail.com>
2020-03-31 23:32:01 +0300

    added basic part to graphql

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,6 +1,14 @@
-import { GraphQLResolveInfo } from "graphql"
+import { GraphQLResolveInfo } from 'graphql'
 // import { Context } from "../models"
-import { IResolvers } from "graphql-tools"
+import { IResolvers } from 'graphql-tools'
+
+type Part = {
+  name: string;
+  number: string;
+  // users: {
+
+  // }
+}
 
 const resolvers: IResolvers = {
   Query: {
@@ -8,11 +16,19 @@ const resolvers: IResolvers = {
       _: void,
       args: void,
       ctx: void,
-      info: GraphQLResolveInfo
+      info: GraphQLResolveInfo,
     ): string {
       return `👋 Hello world! 👋`
-    }
-  }
+    },
+    parts(_: void, args: void, ctx: void, info: GraphQLResolveInfo): Part[] {
+      return [
+        {
+          name: 'Drive Shaft',
+          number: '04',
+        },
+      ]
+    },
+  },
 }
 
 export default resolvers
