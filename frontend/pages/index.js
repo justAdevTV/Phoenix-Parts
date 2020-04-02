750acb2523562a73550b4a86318f490a28e4a761
Justin Punzalan <justpunzz@gmail.com>
2020-04-01 23:04:15 -0700

    Feature Modal - I made a modal with example usage in index

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,21 @@
+import Button from '@material-ui/core/Button';
+import { Modal } from '../components/organisms';
+
 function Home() {
-  return <div>This is the start of something great :)</div>;
+  return (
+    <div>
+      This is the start of something great :)
+      <Modal
+        modalTrigger={
+          <Button variant="contained" color="primary">
+            Modal Trigger
+          </Button>
+        }
+      >
+        Hello
+      </Modal>
+    </div>
+  );
 }
 
 export default Home;
